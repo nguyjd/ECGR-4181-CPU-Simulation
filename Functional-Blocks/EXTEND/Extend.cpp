f9b899ea45538aaeba78fa5ffcb45a133068753e
nguyjd <nguyjd@gmail.com>
2021-10-23 16:11:58 -0400

    Created a base class for the sign extend and zero extend

---
+++ Functional-Blocks/EXTEND/Extend.cpp
@@ -0,0 +1,80 @@
+#include "Extend.h"
+
+Extend::Extend()
+{
+
+	// Connect all wires
+	inputWire = nullptr;
+	outputWire = nullptr;
+	inputBusSize = 0;
+	outputBusSize = 0;
+
+	validConfig = false;
+
+}
+
+Extend::Extend(Wire* input, Wire* output)
+{
+
+	// Set all the wires
+	inputWire = input;
+	outputWire = output;
+	
+	inputBusSize = inputWire->GetAmountofWires();
+	outputBusSize = outputWire->GetAmountofWires();;
+
+	// Check the config.
+	validConfig = CheckConnection();
+
+}
+
+void Extend::ConnectInput(Wire* wire)
+{
+
+	inputWire = wire;
+	inputBusSize = inputWire->GetAmountofWires();
+
+	// Check the config.
+	validConfig = CheckConnection();
+
+}
+
+void Extend::ConnectOutput(Wire* wire)
+{
+
+	outputWire = wire;
+	outputBusSize = outputWire->GetAmountofWires();
+
+	// Check the config.
+	validConfig = CheckConnection();
+
+}
+
+bool Extend::CheckConnection()
+{
+
+	// Check the wire to see if they exist.
+	if (!inputWire || !outputWire)
+	{
+
+		return false;
+
+	}
+
+	// Check all the bus size and check if the data is valid.
+	if (inputWire->GetAmountofWires() != inputBusSize || !inputWire->IsWireDataValid())
+	{
+
+		return false;
+
+	}
+	if (outputWire->GetAmountofWires() != outputBusSize || !outputWire->IsWireDataValid())
+	{
+
+		return false;
+
+	}
+
+	return true;
+
+}
