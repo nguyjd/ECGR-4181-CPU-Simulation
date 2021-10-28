5f96dd78973789ef3ce10c87785c5175370e9ac5
Jonathon Nguyen <nguyjd@gmail.com>
2021-10-28 12:33:31 -0400

    Fixed comments
    Fixed comments

--- Functional-Blocks/SHIFT/ShiftLeft.h
+++ Functional-Blocks/SHIFT/ShiftLeft.h
@@ -11,7 +11,7 @@ public:
 	ShiftLeft();
 	ShiftLeft(Wire* input, Wire* output, long long shiftCount);
 
-	// Connect the wire to the mux and checks if the config is valid.
+	// Connect the wire and checks if the config is valid.
 	void ConnectInput(Wire* wire);
 	void ConnectOutput(Wire* wire);
 
@@ -24,7 +24,7 @@ private:
 	// Check if the size of the bus is correct and if the wire exist.
 	bool CheckConnection();
 
-	// True if the extend is configured correctly.
+	// True if the shift is configured correctly.
 	bool validConfig;
 
 	long long amountToShift;
